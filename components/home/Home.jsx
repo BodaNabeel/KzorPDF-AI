8426874c691fca65bbd9bb25ee975842c14828ff
Nabeel Boda <needtoexplore01@gmail.com>
2023-08-19 19:03:07 +0530

    using getStaticProps to fetch data [temp solution]

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,65 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 import { TrashIcon } from "@heroicons/react/20/solid";
 
-function HomePage() {
-  
-  const DUMMY_DATA = [
-    {
-      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
-      date: "26 July, 2023"
-    },
-    {
-      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
-      date: "26 August, 2023"
-    },
-    {
-      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
-      date: "26 July, 2023"
-    },
-    {
-      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
-      date: "26 August, 2023"
-    },
-    {
-      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
-      date: "26 July, 2023"
-    },
-    {
-      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
-      date: "26 August, 2023"
-    },
-    {
-      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
-      date: "26 July, 2023"
-    },
-    {
-      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
-      date: "26 August, 2023"
-    },
-  ]
+function HomePage(props) {
+  const { data } = props;
+  const [fetchedData, setFetchedData] = useState(data);
+  // console.log(fetchedData);
+  function handler(id) {
+    const tempData = [...fetchedData]
+    tempData.splice(id,1)
+    setFetchedData(tempData);
+  }
+
   return (
-    <div className='lg:my-16'>
-      {DUMMY_DATA.map((data,index) => {
+    <div className="lg:my-16">
+      {fetchedData.map((data, index) => {
         return (
-          <div onClick={() => console.log(index)} className='flex py-4 px-7 justify-between bg-white rounded-lg cursor-pointer bg-gradient-to-r from-[#e2e6f3] to-[#e9e1fa] mb-10'>
-          <div className='border-2 w-[70%]'>
-            <h3 className='font-semibold'>{data.title}
-            
-            </h3>
-          </div>
-          <div className='flex gap-10 items-center'>
-            <div>
-              <p className='font-semibold'>{data.date}</p>
+          <div
+            key={index}
+            onClick={() => console.log(index)}
+            className="flex py-4 px-7 justify-between bg-white rounded-lg cursor-pointer bg-gradient-to-r from-[#e2e6f3] to-[#e9e1fa] mb-10"
+          >
+            <div className="border-2 w-[70%]">
+              <h3 className="font-semibold">{data.title}</h3>
             </div>
-            <div>
-            <TrashIcon className="h-6 w-6 text-gray-500 cursor-pointer hover:text-red-600 transition ease-in-out delay-100 " />
+            <div className="flex gap-10 items-center">
+              <div>
+                <p className="font-semibold">{data.date}</p>
+              </div>
+              <div>
+                <TrashIcon
+                  onClick={() => handler(index)}
+                  className="h-6 w-6 text-gray-500 cursor-pointer hover:text-red-600 transition ease-in-out delay-100 "
+                />
+              </div>
             </div>
           </div>
-        </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,15 @@
+import { DUMMY_DATA } from "@/DUMMY_DATA"
 import HomePage from "../components/home/Home"
-export default function Home() {
+export default function Home(props) {
+    const {data} = props
     return <section className=" min-h-screen max-h-max lg:mx-20 lg:my-10">
-        <HomePage />
+        <HomePage data={data}/>
     </section>
+}
+
+export const getStaticProps = async () => {
+    const data = DUMMY_DATA
+    return {
+        props: {data}
+    }
 }
