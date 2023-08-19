ef240d529eff00d2dab082418f528fa63d8d2dd2
Nabeel Boda <needtoexplore01@gmail.com>
2023-08-19 19:02:37 +0530

    added dummy data

---
+++ DUMMY_DATA.js
@@ -0,0 +1,34 @@
+export const DUMMY_DATA = [
+    {
+      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
+      date: "26 July, 2023"
+    },
+    {
+      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
+      date: "26 August, 2023"
+    },
+    {
+      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
+      date: "26 July, 2023"
+    },
+    {
+      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
+      date: "26 August, 2023"
+    },
+    {
+      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
+      date: "26 July, 2023"
+    },
+    {
+      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
+      date: "26 August, 2023"
+    },
+    {
+      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
+      date: "26 July, 2023"
+    },
+    {
+      title: "PDF: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam veritatis similique aliquid, quos blanditiis doloribus.",
+      date: "26 August, 2023"
+    },
+  ]
