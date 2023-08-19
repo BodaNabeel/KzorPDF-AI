dc3a13bd0ae1c5828026eaa7b0cc6c09579688de
Nabeel Boda <needtoexplore01@gmail.com>
2023-08-19 19:46:24 +0530

    -

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@ import { DUMMY_DATA } from "@/DUMMY_DATA"
 import HomePage from "../components/home/Home"
 export default function Home(props) {
     const {data} = props
-    return <section className=" min-h-screen max-h-max lg:mx-20 lg:my-10">
+    return <section className=" min-h-screen max-h-max w-[60%] lg:mt-32 mx-auto">
         <HomePage data={data}/>
     </section>
 }
