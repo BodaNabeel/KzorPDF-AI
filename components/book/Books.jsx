7e6408ef4662c8aabaf13a0a6c6277aa470fb3c8
Nabeel Boda <needtoexplore01@gmail.com>
2023-08-20 12:36:26 +0530

    ft: book component styled

--- components/book/Books.jsx
+++ components/book/Books.jsx
@@ -1,45 +1,50 @@
-import React from 'react'
+import React from "react";
 
 function BooksPage() {
+  const books = [
+    { name: "A thousand splendid suns", author: "Khaled Hosseini" },
+    { name: "Atomic Habits", author: "James Clear" },
+    { name: "Object Oriented Programming in C++", author: "David Jons" },
+    {
+      name: "We shall never see again how the killed the UI/UX of Google on an awful day",
+      author: "David John Miller Lorem",
+    },
+    { name: "A thousand splendid suns", author: "Khaled Hosseini" },
+    { name: "Atomic Habits", author: "James Clear" },
+    { name: "Object Oriented Programming in C++", author: "David Jons" },
+    {
+      name: "We shall never see again how the killed the UI/UX of Google on an awful day",
+      author: "David John Miller Lorem",
+    },
+    { name: "A thousand splendid suns", author: "Khaled Hosseini" },
+    { name: "Atomic Habits", author: "James Clear" },
+    { name: "Object Oriented Programming in C++", author: "David Jons" },
+    {
+      name: "We shall never see again how the killed the UI/UX of Google on an awful day",
+      author: "David John Miller Lorem",
+    },
+    {
+      name: "The Story",
+      author: "nabeel",
+    },
+  ];
   return (
-    <div className='flex flex-col lg:flex-row gap-5 flex-wrap'>
-    
-        <div className='border-4 flex flex-col  w-[80%] lg:w-[33%] h-80'>
-            <div className='bg-gradient-to-r from-[#e2e6f3] to-[#e9e1fa]   flex flex-col justify-center items-center h-[80%]'>
-
-            <h1 className='font-bold text-2xl'>Think & Grow Rich</h1>
-            <p className='text-gray-500 font-se'>By: Nepolean Hills</p>
-            </div>
-            <div className='flex w-[100%] mx-auto h-[20%] justify-around'>
-                <button>Open</button>
-                <button>Delete</button>
-            </div>
-        </div>
-        <div className='border-4 flex flex-col  w-[80%] lg:w-[33%] h-80'>
-            <div className='bg-gradient-to-r from-[#e2e6f3] to-[#e9e1fa]   flex flex-col justify-center items-center h-[80%]'>
-
-            <h1 className='font-bold text-2xl'>Think & Grow Rich</h1>
-            <p className='text-gray-500 font-se'>By: Nepolean Hills</p>
-            </div>
-            <div className='flex w-[100%] mx-auto h-[20%] justify-around'>
-                <button>Open</button>
-                <button>Delete</button>
-            </div>
-        </div>
-        <div className='border-4 flex flex-col  w-[80%] lg:w-[33%] h-80'>
-            <div className='bg-gradient-to-r from-[#e2e6f3] to-[#e9e1fa]   flex flex-col justify-center items-center h-[80%]'>
-
-            <h1 className='font-bold text-2xl'>Think & Grow Rich</h1>
-            <p className='text-gray-500 font-se'>By: Nepolean Hills</p>
-            </div>
-            <div className='flex w-[100%] mx-auto h-[20%] justify-around'>
-                <button>Open</button>
-                <button>Delete</button>
-            </div>
-        </div>
-       
-        </div>
-  )
+    <div className="flex flex-col lg:flex-row gap-28 flex-wrap">
+      {books.map((book, index) => {
+        return (
+          <div
+            key={index}
+            className="flex flex-col w-48 bg-[#8dd384] rounded-tl-lg rounded-bl-lg rounded-tr-2xl rounded-br-2xl h-72 items-center justify-around cursor-pointer shadow-[0px_0px_15px_2px_#8dd384]"
+          >
+            <h1 className="font-bold text-3xl h-[70%] w-[80%] text-white overflow-hidden mx-auto leading-snug line-clamp-4">
+              {book.name}
+            </h1>
+            <p className="text-[#275121] font-semibold"> {book.author}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default BooksPage
+export default BooksPage;
--- pages/books.js
+++ pages/books.js
@@ -1,7 +1,7 @@
 import BooksPage from "../components/book/Books"
 export default function Books() {
     return (
-    <section className=" min-h-screen max-h-max w-[60%] lg:mt-32 mx-auto">
+    <section className=" min-h-screen max-h-max w-[80%] lg:mt-32 mx-auto">
     <BooksPage/>
     </section>
 
