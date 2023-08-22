63fc61870efd0e625872c9b89b7bda588b174d6d
Nabeel Boda <needtoexplore01@gmail.com>
2023-08-22 11:44:56 +0530

    width decreased for mobile deviced

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -2,7 +2,7 @@ import BookmarkPage from "@/components/bookmark/Bookmark";
 
 export default function Bookmark() {
   return (
-    <section className=" min-h-screen max-h-max lg:w-[80%] w-[100%] mt-20 lg:mt-32 mx-auto ">
+    <section className=" min-h-screen max-h-max lg:w-[80%] w-[90%] mt-20 lg:mt-32 mx-auto ">
       <BookmarkPage />
     </section>
   );
