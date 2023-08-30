88317921201eab7a9186ee3fbcfbbf1368a5b6c0
Nabeel Boda <needtoexplore01@gmail.com>
2023-08-30 17:06:28 +0530

    created new page routes

---
+++ pages/summarizepdf.js
@@ -0,0 +1,9 @@
+import BookmarkPage from "@/components/bookmark/Bookmark";
+
+export default function Bookmark() {
+  return (
+    <section className=" min-h-screen max-h-max lg:w-[80%] w-[90%] mt-20 lg:mt-32 mx-auto ">
+      <BookmarkPage />
+    </section>
+  );
+}
