1fc34623cdb74e7621feee4080874e105e309e8d
Nabeel Boda <needtoexplore01@gmail.com>
2023-08-31 21:04:00 +0530

    modified style

--- pages/library/index.js
+++ pages/library/index.js
@@ -1,8 +1,8 @@
-import BooksPage from "../../components/book/Books";
+import Library from "@/components/library/Library";
 export default function Books() {
   return (
-    <section className=" min-h-screen max-h-max lg:w-[80%] mt-20 lg:mt-32 mx-auto">
-      <BooksPage />
+    <section className="   w-[90%] mt-20  mx-auto">
+      <Library />
     </section>
   );
 }
