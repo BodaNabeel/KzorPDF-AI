d27ed24fd5d11ab1e27acf28bb12d5ab8352717f
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-05 12:26:44 +0530

    delete collection functionality

--- components/library/CollectionItems.jsx
+++ components/library/CollectionItems.jsx
@@ -4,13 +4,23 @@ import Image from "next/image";
 import { IconCircleMinus } from "@tabler/icons-react";
 
 function Folder(props) {
-  const { selectedCollection, collection } = props;
-  const [collectionMenu, setCollectionMenu] = useState(false);
+  const {
+    collection,
+    setCollection,
+    selectedCollection,
+    setSelectedCollection,
+  } = props;
+  function deleteCollection() {
+    let arr = [...collection];
+    arr.splice(selectedCollection, 1);
+    setCollection(arr);
+    setSelectedCollection(0);
+  }
   function DisplayCollectionItems() {
-    if (collection[selectedCollection].collectionItems.length <= 0) {
+    if (collection[selectedCollection]?.collectionItems.length <= 0) {
       return <h1>No items have been found mate</h1>;
     } else {
-      return collection[selectedCollection].collectionItems.map(
+      return collection[selectedCollection]?.collectionItems.map(
         (element, index) => {
           return (
             <div
@@ -18,15 +28,19 @@ function Folder(props) {
               className="flex cursor-pointer justify-between px-4 py-2 hover:bg-gray-100 transition-all duration-300"
             >
               <div className="flex gap-4">
-                <Image src="/images/pdf.svg" height={24} width={24} />
+                <Image
+                  src="/images/pdf.svg"
+                  height={24}
+                  width={24}
+                  alt="image of pdf"
+                />
                 <span>
                   <p className="font-medium">{element}</p>
                   <p className="text-sm text-gray-500">26 July, 2023</p>
                 </span>
               </div>
-
               <span className="text-s_grey-600 cursor-pointer self-center">
-              <IconCircleMinus/>
+                <IconCircleMinus />
               </span>
             </div>
           );
@@ -40,19 +54,19 @@ function Folder(props) {
       <div className="border-b-[1px] flex px-4 py-2  items-center justify-between">
         <span className=" ">
           <p className="text-lg font-medium">
-            {collection[selectedCollection].collectionName}
+            {collection[selectedCollection]?.collectionName}
           </p>
           <p className="text-s_grey-600 text-sm font-medium">
-            {collection[selectedCollection].collectionItems.length} items
+            {collection[selectedCollection]?.collectionItems.length} items
           </p>
         </span>
-      
-          <span
-          onClick={()=> setCollectionMenu(true)}
-          className="text-s_grey-600 cursor-pointer">
-            <IconTrashX/>
-          </span>
-       
+
+        <span
+          onClick={deleteCollection}
+          className="text-s_grey-600 cursor-pointer"
+        >
+          <IconTrashX />
+        </span>
       </div>
       <div className="py-2">{<DisplayCollectionItems />}</div>
     </div>
