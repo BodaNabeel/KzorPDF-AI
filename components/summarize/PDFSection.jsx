78320e949af0ab490aa2d7bd1bf8329e2cefc075
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-12 12:07:10 +0530

    added icon for download

--- components/summarize/PDFSection.jsx
+++ components/summarize/PDFSection.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import PageSelector from "./tools/PageSelector";
 import PDFMode from "./tools/PDFMode";
+import { IconDownload } from "@tabler/icons-react";
 export default function PDFSection() {
   return (
     <div className=" h-96 mb-5 lg:h-full lg:w-[50%] border-r-4 lg:border-gray-600 ">
       <div className="  flex justify-between px-2 py-2 h-[5%] items-center">
         <PageSelector />
         <PDFMode />
-        <button>Download PDF</button>
+        <button className="flex   text-accent-400 border-[2px] border-accent-400  font-medium px-2 py-1 rounded-md">
+          <IconDownload height={20} />
+          <p>Download</p>
+        </button>
       </div>
       <div className="h-[95%] overflow-y-auto">
         <object
