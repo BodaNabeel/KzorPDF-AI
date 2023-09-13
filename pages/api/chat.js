d9c676fb263263d7b8144988f7461c02c329cfe7
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-13 22:01:13 +0530

    built API for interacting with gpt

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,14 +1,22 @@
 import openai from "@/utils/Configuration";
-import { NextApiRequest, NextApiResponse } from "next";
 export default async function (req, res) {
-  const completion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: "What is 2+2?" },
-    ],
-  });
+  const { method, body } = req;
 
-  const response = completion;
-  res.status(200).json(response);
+  switch (method) {
+    case "POST":
+      const userMessage = body.message;
+      const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: userMessage },
+        ],
+      });
+
+      const response = completion.choices[0];
+      return res.status(200).json(response);
+
+    default:
+      return res.status(400).json({ error: "Method not Allowed" });
+  }
 }
