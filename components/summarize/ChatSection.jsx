e28f8779b3f6c0a7563b208bf5013ad61d2280ac
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-13 22:00:52 +0530

    integrated chat with gpt

--- components/summarize/ChatSection.jsx
+++ components/summarize/ChatSection.jsx
@@ -1,40 +1,50 @@
 import React, { useEffect, useRef, useState } from "react";
-
 import { IconSend } from "@tabler/icons-react";
-
 function ChatSection() {
-  const DUMMY_DATA = [
-    {
-      text: "Welcome to the AI model - a single place to ask all your queries related to a PDF/Book. It has been built by Nabeel Boda & will love to listen to your feedback. heya! like share & subscribe to our x.com/bodanabeel :)",
-      user: false,
-    },
-    {
-      text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quibusdam corrupti ad nihil praesentium. Praesentium eos, aut odit obcaecati nobis veniam, et minus ad voluptate quam iste adipisci, maxime libero quos explicabo.  ectetur adipisicing elit. Veritatis quibusdam corrupti ad nihil praesentium. Praesentium eos, aut odit obcaecati nobis veniam, et minus ad voluptate",
-      user: true,
-    },
-    {
-      text: "Welcome to the AI model - a single place to ask all your queries related to a PDF/Book. It has been built by Nabeel Boda & will love to listen to your feedback. heya! like share & subscribe to our x.com/bodanabeel :)",
-      user: false,
-    },
-    {
-      text: "Welcome to the AI model - a single place to ask all your queries related to a PDF/Book. It has been built by Nabeel Boda & will love to listen to your feedback. heya! like share & subscribe to our x.com/bodanabeel :)",
-      user: false,
-    },
-    {
-      text: "ectetur adipisicing elit. Veritatis quibusdam corrupti ad nihil praesentium. Praesentium eos, aut odit obcaecati nobis veniam, et minus ad voluptate ectetur adipisicing elit. Veritatis quibusdam corrupti ad nihil praesentium. Praesentium eos, aut odit obcaecati nobis veniam, et minus ad voluptate ectetur adipisicing elit. Veritatis quibusdam corrupti ad nihil praesentium. Praesentium eos, aut odit obcaecati nobis veniam, et minus ad voluptate ectetur adipisicing elit. Veritatis quibusdam corrupti ad nihil praesentium. Praesentium eos, aut odit obcaecati nobis veniam, et minus ad voluptate",
-      user: true,
-    },
-  ];
-  const inputRef = useRef();
-  const chatContainerRef = useRef();
-  const [chat, setChat] = useState(DUMMY_DATA);
+  const inputRef = useRef(null);
+  const chatContainerRef = useRef(null);
+  const [chat, setChat] = useState([]);
+  const [responseFromGPT, setResponseFromGPT] = useState(null);
   function scrollToBottom() {
     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
   }
-  useEffect(() => {
-    scrollToBottom();
-  }, []);
+  function updateChat(response, isUser) {
+    console.log(chat, chat.length);
+    let arr = [];
+    if (chat.length > 0) {
+      console.log("run");
+      setChat((currentState) => [
+        ...currentState,
+        {
+          text: response,
+          user: isUser,
+        },
+      ]);
+    } else {
+      console.log("sudo run");
 
+      setChat([{ text: response, user: isUser }]);
+    }
+  }
+  function updateClientMessage() {
+    if (inputRef.current.value) {
+      updateChat(inputRef.current.value, true);
+    }
+  }
+  const fetchMessage = async () => {
+    const userMessage = inputRef.current.value;
+
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: userMessage }),
+    });
+
+    const data = await response.json();
+    updateChat(data.message.content, false);
+  };
   useEffect(() => {
     scrollToBottom();
   }, [chat]);
@@ -44,28 +54,29 @@ function ChatSection() {
         ref={chatContainerRef}
         className="overflow-y-auto flex flex-col  px-4 pt-2 h-[90%] scroll-smooth"
       >
-        {chat.map((data, index) => {
-          if (!data.user) {
-            return (
-              <div
-                key={index}
-                className="bg-[#f9f9fe] mb-5 w-[77%] self-start rounded-md px-4 py-2 border-[1px] rounded-tl-none"
-              >
-                <h1>{data.text}</h1>
-              </div>
-            );
-          }
-          if (data.user) {
-            return (
-              <div
-                key={index}
-                className="bg-primary-400 text-white mb-5 w-[77%] self-end rounded-md px-4 py-4 rounded-tr-none border-[1px]"
-              >
-                <h1>{data.text}</h1>
-              </div>
-            );
-          }
-        })}
+        {chat.length > 0 &&
+          chat.map((data, index) => {
+            if (!data.user) {
+              return (
+                <div
+                  key={index}
+                  className="bg-[#f9f9fe] mb-5 w-[77%] self-start rounded-md px-4 py-2 border-[1px] rounded-tl-none"
+                >
+                  <h1>{data.text}</h1>
+                </div>
+              );
+            }
+            if (data.user) {
+              return (
+                <div
+                  key={index}
+                  className="bg-primary-400 text-white mb-5 w-[77%] self-end rounded-md px-4 py-4 rounded-tr-none border-[1px]"
+                >
+                  <h1>{data.text}</h1>
+                </div>
+              );
+            }
+          })}
       </div>
       <div className="flex h-[5%]  w-[95%] mx-auto  overflow-hidden gap-2 pb-1">
         <input
@@ -77,10 +88,8 @@ function ChatSection() {
 
         <button
           onClick={() => {
-            let arr = [...chat];
-            arr.push({ text: inputRef.current.value, user: true });
-            setChat(arr);
-            console.log(chat);
+            updateClientMessage();
+            fetchMessage();
           }}
           className="bg-primary-400 text-white py-1 px-4"
         >
