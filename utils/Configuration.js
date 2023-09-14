1b4275fd8a8df96e34cee756f7ef78a7a605ffda
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-14 10:09:03 +0530

    -

--- utils/Configuration.js
+++ utils/Configuration.js
@@ -1,7 +1,4 @@
 import OpenAI from "openai";
-// const configuration = new Configuration({
-//   apiKey: "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",
-// });
 const openai = new OpenAI({
   api_key: process.env.OPENAI_API_KEY,
 });
