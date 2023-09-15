1b6b46577422a084fd9f160ac61f6f5b6b062259
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-15 12:15:26 +0530

    fetching pdf from filesystem

--- pages/summarize/index.js
+++ pages/summarize/index.js
@@ -1,9 +1,27 @@
 import Summarize from "@/components/summarize/Summarize";
+import { DataContext } from "@/context/context";
+import { useContext, useEffect } from "react";
+import fs from "fs";
+import path from "path";
+import pdf from "pdf-parse";
+export default function SummarizePage({ document_text }) {
+  const { document, setDocument } = useContext(DataContext);
+  useEffect(() => {
+    setDocument(document_text);
+  }, []);
 
-export default function SummarizePage() {
   return (
     <section className=" h-screen   mx-auto ">
       <Summarize />
     </section>
   );
+}
+
+export async function getServerSideProps() {
+  const filePath = path.join(process.cwd(), "public", "final_report.pdf");
+  const dataBuffer = fs.readFileSync(filePath);
+  const document_text = await pdf(dataBuffer).then((data) => {
+    return data.text;
+  });
+  return { props: { document_text } };
 }
