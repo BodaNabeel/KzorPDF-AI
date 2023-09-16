4f9eaae3f53aa621d81413d4ff1f31536749c3a4
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-16 14:23:19 +0530

    -

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -5,15 +5,23 @@ export default async function (req, res) {
   switch (method) {
     case "POST":
       const userMessage = body.message;
+      const document = body.doc;
       const completion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [
-          { role: "system", content: "You are a helpful assistant." },
+          {
+            role: "system",
+            content:
+              "Act as an assistant who analyzes large documents and then answer queries regarding it and is able to summarize, point of critical information and better insights from the document.",
+          },
           { role: "user", content: userMessage },
+          { role: "assistant", content: document },
         ],
       });
 
       const response = completion.choices[0];
+      console.log(document);
+      // console.log(response);
       return res.status(200).json(response);
 
     default:
