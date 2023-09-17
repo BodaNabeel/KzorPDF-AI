d47669ac0b23c818032b3cbc6d53c2db889dd678
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-17 20:32:01 +0530

    -

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,27 +1,30 @@
 import openai from "/utils/configuration";
+
 export default async function (req, res) {
   const { method, body } = req;
 
   switch (method) {
     case "POST":
       const userMessage = body.message;
+      const customMsg =
+        "From the provided document, answer this query: " + userMessage;
       const document = body.doc;
-      console.log(document);
+      const messages = [
+        {
+          role: "system",
+          content:
+            "Act as an assistant who analyzes large documents and then answers queries regarding it and is able to summarize, point out critical information and provide better insights from the document.",
+        },
+        { role: "user", content: customMsg },
+        { role: "assistant", content: document },
+      ];
+
       const completion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "system",
-            content:
-              "Act as an assistant who analyzes large documents and then answer queries regarding it and is able to summarize, point of critical information and better insights from the document.",
-          },
-          { role: "user", content: userMessage },
-          { role: "assistant", content: document },
-        ],
+        messages: messages,
       });
 
       const response = completion.choices[0];
-      console.log(response);
       return res.status(200).json(response);
 
     default:
