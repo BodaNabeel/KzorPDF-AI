fb682053887b980918a4c213aabdefa7b94ff88c
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-17 12:26:35 +0530

    changed the doc

--- components/summarize/PDFSection.jsx
+++ components/summarize/PDFSection.jsx
@@ -15,14 +15,14 @@ export default function PDFSection() {
       </div>
       <div className="h-[95%] overflow-y-auto">
         <object
-          data="/final_report.pdf"
+          data="/research.pdf"
           type="application/pdf"
           width="100%"
           height="100%"
         >
           <p>
             Alternative text - include a link{" "}
-            <a href="/final_report.pdf">to the PDF!</a>
+            <a href="/research.pdf">to the PDF!</a>
           </p>
         </object>
       </div>
