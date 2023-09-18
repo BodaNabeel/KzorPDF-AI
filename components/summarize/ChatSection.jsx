666351de5f422103424424d05cc7e90eb635cc29
Nabeel Boda <needtoexplore01@gmail.com>
2023-09-18 11:19:05 +0530

    custom textarea height

--- components/summarize/ChatSection.jsx
+++ components/summarize/ChatSection.jsx
@@ -4,21 +4,21 @@ import { DataContext } from "@/context/context";
 function ChatSection() {
   function Loading() {
     return (
-      <div class="chat-bubble">
-        <div class="typing">
-          <div class="dot"></div>
-          <div class="dot"></div>
-          <div class="dot"></div>
+      <div className="chat-bubble">
+        <div className="typing">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
         </div>
       </div>
     );
   }
   const { document } = useContext(DataContext);
-  const inputRef = useRef(null);
   const chatContainerRef = useRef(null);
   const [chat, setChat] = useState([]);
   const [responding, setResponding] = useState(false);
-
+  const [value, setValue] = useState(undefined);
+  const inputRef = useRef(null);
   function scrollToBottom() {
     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
   }
@@ -31,21 +31,23 @@ function ChatSection() {
   }
 
   function updateClientMessage() {
-    if (inputRef.current.value) {
-      updateChat(inputRef.current.value, true);
-      fetchMessage();
+    if (value) {
+      setValue(null);
+      setResponding(true);
+      const message = inputRef.current.value;
+      updateChat(message, true);
+      fetchMessage(message);
+      inputRef.current.value = "";
     }
   }
 
-  const fetchMessage = async () => {
-    const userMessage = inputRef.current.value;
-
+  const fetchMessage = async (value) => {
     const response = await fetch("/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ message: userMessage, doc: document }),
+      body: JSON.stringify({ message: value, doc: document }),
     });
 
     const data = await response.json();
@@ -57,6 +59,15 @@ function ChatSection() {
   useEffect(() => {
     scrollToBottom();
   }, [chat]);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.style.height = "0px";
+      const scrollHeight = inputRef.current.scrollHeight;
+
+      inputRef.current.style.height = scrollHeight + "px";
+    }
+  }, [inputRef.current, value]);
 
   return (
     <div className=" bg-white flex flex-col justify-between h-full ">
@@ -92,20 +103,26 @@ function ChatSection() {
           })}
         {responding ? <Loading /> : null}
       </div>
-      <div className="flex h-[5%]  w-[95%] mx-auto  overflow-hidden gap-2 pb-1">
-        <input
+
+      <div className="flex items-center overflow-hidden  border-2  border-s_grey-50 px-2 py-2 mb-2 rounded-lg shadow-[0px_48px_100px_10px_#110c2e26] mx-5">
+        <textarea
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
           ref={inputRef}
-          type="text"
-          className="w-[95%] border-[2px] outline-none pl-3 border-r-gray-400 bg-white"
-          placeholder="Ask your question..."
-        />
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              updateClientMessage();
+              event.preventDefault();
+            }
+          }}
+          className="w-[95%] max-h-[100%]  outline-none pl-3 border-r-gray-400 bg-white resize-none"
+        ></textarea>
 
         <button
           onClick={() => {
             updateClientMessage();
-            setResponding(true);
           }}
-          className="bg-primary-400 text-white py-1 px-4"
+          className="bg-primary-400 rounded-md text-white py-2 px-2 self-end"
         >
           <IconSend />
         </button>
