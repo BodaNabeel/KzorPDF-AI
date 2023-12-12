40af401e376a0f3b1a7881884e8ebb91279b5b08
Nabeel Boda <needtoexplore01@gmail.com>
2023-12-13 03:35:28 +0530

    tried implementing same but gives bugs

--- middleware.js
+++ middleware.js
@@ -1,13 +1,14 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
+import supabase from "./config/supabaseClient";
 
 export async function middleware(req) {
   const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
+  // const supabase = createMiddlewareClient({ req, res });
 
   const {
     data: { user },
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getSession();
   if (!user) {
     return NextResponse.redirect(new URL("/auth", req.url));
   }
