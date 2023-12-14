95c74cb549e4748b6010b30dd6e015504e277c3d
Nabeel Boda <needtoexplore01@gmail.com>
2023-12-14 22:37:20 +0530

    -

--- components/summarize/Summarize.jsx
+++ components/summarize/Summarize.jsx
@@ -4,6 +4,8 @@ import ChatSection from "./ChatSection";
 import Header from "@/utils/Header";
 import NoteSection from "./NoteSection";
 import { useSession } from "next-auth/react";
+import supabase from "@/config/supabaseClient";
+
 function Summarize() {
   const [selectedOption, setSelectedOption] = useState(0);
   const data = useSession();
@@ -20,6 +22,11 @@ function Summarize() {
   //   const data = await response.json();
   //   console.log(data);
   // };
+  const supabase_demo = async () => {
+    const { error } = await supabase.from("users_new").insert({ id: 2 });
+    // const { data, error } = await supabase.from("demo").select();
+    console.log(error);
+  };
   return (
     <section className="lg:h-screen h-max  w-[100%] flex flex-col lg:flex-row">
       <PDFSection />
@@ -31,7 +38,7 @@ function Summarize() {
           {selectedOption === 0 ? <ChatSection /> : <NoteSection />}
         </div>
       </Header>
-      <button onClick={() => console.log(data)}>Click me</button>
+      <button onClick={supabase_demo}>Click me</button>
       {/* <button onClick={supabase_demo}>Click me</button> */}
     </section>
   );
