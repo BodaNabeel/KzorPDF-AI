154c43b086e7cc864352c643719067664c5a1483
Nabeel Boda <needtoexplore01@gmail.com>
2023-12-19 19:54:47 +0530

    create_folder api completed

--- pages/api/create_folder.js
+++ pages/api/create_folder.js
@@ -1,4 +1,3 @@
-// Creating a new supabase server client object (e.g. in API route):
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 
 export default async (req, res) => {
@@ -7,10 +6,31 @@ export default async (req, res) => {
     req,
     res,
   });
-
-  const { data } = await supabaseServerClient
-    .from("folder")
-    .insert({ folder_name: body.input });
-  console.log(body.input);
-  res.status(200).json({ name: user?.name ?? "" });
+  const user = await supabaseServerClient.auth.getUser();
+  const user_id = user.data.user.id;
+  switch (method) {
+    case "POST":
+      try {
+        const { data: folderData, error: folderDataError } =
+          await supabaseServerClient
+            .from("folder")
+            .select()
+            .eq("user_id", user_id)
+            .ilike("folder_name", body.input);
+        if (folderData.length === 0) {
+          const { data, error } = await supabaseServerClient
+            .from("folder")
+            .insert({ folder_name: body.input });
+          return res
+            .status(200)
+            .json({ message: "Folder inserted successfully" });
+        } else {
+          return res
+            .status(400)
+            .json({ message: "Folder name already exist." });
+        }
+      } catch (error) {
+        return res.status(500).json({ error: error });
+      }
+  }
 };
