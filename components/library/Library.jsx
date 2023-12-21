00fcdfe4c085d3d00ac5430e95e973faa54ab165
Nabeel Boda <needtoexplore01@gmail.com>
2023-12-21 15:49:37 +0530

    using real data for UI

--- components/library/Library.jsx
+++ components/library/Library.jsx
@@ -4,34 +4,7 @@ import Popup from "./Popup";
 import CollectionItems from "./CollectionItems";
 function Library(props) {
   const { folderData } = props;
-  const tempCollection = [
-    {
-      collectionName: "Untitled collection",
-      collectionItems: [
-        "NTCC Semester II, A2305222027 - Nabeel Boda",
-        "Medical Bills 2022-23",
-        "Semester fee receipt II",
-      ],
-    },
-    {
-      collectionName: "Physics",
-      collectionItems: ["Motion in 1D", "Motion in 2D", "Gravitation"],
-    },
-    {
-      collectionName: "Javascript",
-      collectionItems: [
-        "Arrays",
-        "Functions",
-        "Closure",
-        "Higher order function",
-      ],
-    },
-    {
-      collectionName: "Hackathons",
-      collectionItems: ["Google Apr 23", "Meta May 23", "X Jan 24"],
-    },
-  ];
-  const [collection, setCollection] = useState(tempCollection);
+  const [collection, setCollection] = useState(folderData);
   const [displayPopup, setDisplayPopup] = useState(true);
   const [selectedCollection, setSelectedCollection] = useState(0);
 
