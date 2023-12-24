e6d551073763f6dc07b13c5bdb26f8eeb55e7af3
Nabeel Boda <needtoexplore01@gmail.com>
2023-12-24 23:54:12 +0530

    -

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+        },
+      },
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
--- pages/summarize/index.js
+++ pages/summarize/index.js
@@ -5,7 +5,11 @@ import fs from "fs";
 import path from "path";
 import pdf from "pdf-parse";
 import NavbarLayout from "../../layout/NavbarLayout";
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import {
+  SupabaseClient,
+  createPagesServerClient,
+} from "@supabase/auth-helpers-nextjs";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 
 export default function SummarizePage({
   document_text,
@@ -13,8 +17,47 @@ export default function SummarizePage({
   bookmarkData,
 }) {
   const { setDocumentData, setChatData, setBookmark } = useContext(DataContext);
+  const [pdfLink, setPdfLink] = useState();
+  // async function uploadDoc() {
+  //   const { data, error } = await supabase.storage
+  //     .from("files")
+  //     .upload("document69.pdf", "/final_report.pdf", {
+  //       cacheControl: "3600",
+  //       upsert: false,
+  //     });
+  //   console.log(data);
+  //   console.log(error);
+  // }
+  // useEffect(() => {
+  //   uploadDoc();
+  // }, []);
+  const supabaseClient = useSupabaseClient();
+  const uploadFile = async (event) => {
+    const file = event.target.files[0];
+    const bucket = "files";
 
-  const temporaryID = "abc";
+    // Call Storage API to upload file
+    const { data, error } = await supabaseClient.storage
+      .from(bucket)
+      // .upload(file.name, file);
+      .upload(`public/${file.name}`, file);
+
+    // Handle error if upload failed
+    if (error) {
+      // alert("Error uploading file.");
+      console.log(error);
+      return;
+    }
+
+    alert("File uploaded successfully!");
+  };
+  const getFile = async () => {
+    const publicUrl = supabaseClient.storage
+      .from("files/public")
+      .getPublicUrl("nabeel_resume.pdf");
+    console.log(publicUrl);
+    setPdfLink(publicUrl.data.publicUrl);
+  };
   useEffect(() => {
     if (document_text) {
       setDocumentData(document_text);
@@ -26,6 +69,16 @@ export default function SummarizePage({
   return (
     <NavbarLayout>
       <Summarize />
+      <div>
+        <h1>Upload Profile Photo</h1>
+        <input type="file" onChange={uploadFile} />
+      </div>
+      <button onClick={getFile}>get file</button>
+      <object data={pdfLink} type="application/pdf" width="100%" height="100%">
+        <p>
+          Alternative text - include a link <a href={pdfLink}>to the PDF!</a>
+        </p>
+      </object>
     </NavbarLayout>
   );
 }
@@ -50,12 +103,21 @@ export async function getServerSideProps(context) {
     .eq("user_id", user_id);
 
   // Bookmark
-
   const { data: bookmarkData, error: bookmarkDataError } = await supabase
     .from("chat")
     .select()
     .eq("user_id", user_id)
     .eq("is_bookmarked", true);
+
+  // Storage
+
+  // const file = "final_report.pdf";
+
+  // const { data, error } = await supabase.storage
+  //   .from("files")
+  //   .upload("public/document69.pdf", filePath);
+  // console.log(data);
+  // console.log(error);
 
   return { props: { document_text, chatData, bookmarkData } };
 }
