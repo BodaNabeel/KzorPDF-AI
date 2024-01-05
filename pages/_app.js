a39db62fd3716db955f8082f8fb8f111fa4ba40f
Nabeel Boda <needtoexplore01@gmail.com>
2024-01-05 21:38:29 +0530

    -

--- pages/_app.js
+++ pages/_app.js
@@ -42,29 +42,9 @@ export default function MyApp({ Component, pageProps }) {
       <DataProvider>
         <main>
           <Toaster position="bottom-right" reverseOrder={false} />
-          {/* <LoadingBar
-            color="rgb(180, 130, 251)"
-            progress={progress}
-            waitingTime={400}
-            onLoaderFinished={() => {
-              setProgress(0);
-            }}
-          /> */}
           <Component {...pageProps} />
         </main>
       </DataProvider>
     </SessionContextProvider>
   );
 }
-supabase.auth.onAuthStateChange((event, session) => {
-  if (event === "SIGNED_OUT" || event === "USER_DELETED") {
-    // delete cookies on sign out
-    const expires = new Date(0).toUTCString();
-    document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
-    document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
-  } else if (event === "SIGNED_IN" || event === "TOKEN_REFRESHED") {
-    const maxAge = 100 * 365 * 24 * 60 * 60; // 100 years, never expires
-    document.cookie = `my-access-token=${session.access_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`;
-    document.cookie = `my-refresh-token=${session.refresh_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`;
-  }
-});
