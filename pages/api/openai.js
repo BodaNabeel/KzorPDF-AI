daf971db769b8d6e67ac4d4a7f0607caf7a023fa
Nabeel Boda <needtoexplore01@gmail.com>
2024-01-12 18:55:33 +0530

    test-ing new ai way out

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -1,28 +1,25 @@
-import { OpenAIStream, StreamingTextResponse, streamToResponse } from "ai";
-import { NextApiRequest, NextApiResponse } from "next";
-import OpenAI from "openai";
-
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+import openai from "/config/openai";
 
 export default async function handler(req, res) {
-  const { messages } = await req.body;
+  const { prompt } = await req.body;
+  let reply = [];
 
-  // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: true,
-    messages,
+    messages: [{ role: "user", content: prompt }],
   });
+  for await (const chunk of response) {
+    reply = reply + chunk.choices[0].delta.content;
+    // reply.push(chunk.choices[0].delta.content);
+  }
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-
-  /**
-   * Converts the stream to a Node.js Response-like object.
-   * Please note that this sends the response as one message once it's done.
-   */
-  return streamToResponse(stream, res);
+  console.log(reply);
+  // const filteredArrayOfResponse = reply.pop();
+  // console.log(reply);
+  // const replyFromAI = filteredArrayOfResponse.join(" ");
+  // console.log(replyFromAI);
+  // console.log()
+  // console.log(reply);
+  return res.status(200).json(reply);
 }
--- pages/chat.js
+++ pages/chat.js
@@ -1,24 +1,40 @@
 import { useChat } from "ai/react";
+import { useRef } from "react";
 
 // Optional but recommended: use the Edge Runtime. This can only be done at the page level, not inside nested components.
 // export const runtime = "experimental-edge";
 
 export default function IndexPage() {
-  const { messages, handleSubmit, input, handleInputChange } = useChat();
+  const ref = useRef();
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(ref.current.value);
+    const response = await fetch("api/openai", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: ref.current.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.replace(/\.undefined$/, ""));
+      });
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <label for="input">Prompt</label>
       <input
         name="prompt"
-        value={input}
-        onChange={handleInputChange}
+        // value={input}
+        // onChange={handleInputChange}
         id="input"
+        ref={ref}
       />
       <button type="submit">Submit</button>
-      {messages.map((message, i) => (
-        <div key={i}>{message.content}</div>
-      ))}
     </form>
   );
 }
