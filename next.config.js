9b492aa052236e5690ec7541c00117bb377909ab
Nabeel Boda <needtoexplore01@gmail.com>
2024-01-13 11:28:57 +0530

    streaming

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 // }
 module.exports = {
   reactStrictMode: true,
+
   webpack: (config) => {
     config.module.rules.push({
       test: /\.pdf$/,
--- pages/chat.js
+++ pages/chat.js
@@ -1,40 +1,67 @@
 import { useChat } from "ai/react";
-import { useRef } from "react";
-
-// Optional but recommended: use the Edge Runtime. This can only be done at the page level, not inside nested components.
-// export const runtime = "experimental-edge";
+import { useRef, useState } from "react";
+import {
+  createParser,
+  ParsedEvent,
+  ReconnectInterval,
+} from "eventsource-parser";
 
 export default function IndexPage() {
+  const [respond, setRespond] = useState("yolo");
   const ref = useRef();
-  async function handleSubmit(e) {
+
+  const handleSubmit = async (e) => {
+    const prompt = ref.current.value;
     e.preventDefault();
-    console.log(ref.current.value);
-    const response = await fetch("api/openai", {
+    const response = await fetch("/api/openai", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        prompt: ref.current.value,
+        prompt,
       }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.replace(/\.undefined$/, ""));
-      });
-  }
+    });
+
+    if (!response.ok) {
+      console.log(response.statusText);
+    }
+
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+    const onParseGPT = (event) => {
+      if (event.type === "event") {
+        const data = event.data;
+        try {
+          const text = JSON.parse(data).text ?? "";
+          setRespond((prev) => prev + text);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    };
+
+    const onParse = onParseGPT;
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    const parser = createParser(onParse);
+    let done = false;
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      parser.feed(chunkValue);
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label for="input">Prompt</label>
-      <input
-        name="prompt"
-        // value={input}
-        // onChange={handleInputChange}
-        id="input"
-        ref={ref}
-      />
-      <button type="submit">Submit</button>
+      <label htmlFor="input">Prompt</label>
+      <p> {respond}</p>
+      <input name="prompt" id="input" ref={ref} />
+      <button type={handleSubmit}>Submit</button>
     </form>
   );
 }
