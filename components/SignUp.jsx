7be0bad652152b89a18f45c1fe2342f6b90fb4a1
Nabeel Boda <needtoexplore01@gmail.com>
2024-01-21 20:21:13 +0530

    Test: kuch to ho raha hai

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -3,13 +3,25 @@ import Image from "next/image";
 
 export default function SignUp() {
   const supabaseClient = useSupabaseClient();
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+  };
+
   async function handleLogin() {
     const { data, error } = await supabaseClient.auth.signInWithOAuth({
       provider: "google",
       options: {
         queryParams: {
           access_type: "offline",
-          redirectTo: window.location.origin + "/home",
+          redirectTo: getURL(),
         },
       },
     });
