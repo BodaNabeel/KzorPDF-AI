6a2443932fe5a4c66b07b9f87c5052edc8aa6c3a
Nabeel Boda <needtoexplore01@gmail.com>
2024-01-22 20:56:24 +0530

    Test: Using home as "/" & "/guest"

---
+++ pages/guest.js
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+import supabase from "/config/supabaseClient";
+
+export default function Home() {
+  return <h1>Hello</h1>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,42 @@
-import { useEffect, useState } from "react";
-import supabase from "/config/supabaseClient";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import { DUMMY_DATA } from "../../DUMMY_DATA";
+import HomePage from "../../components/home/Home";
+import NavbarLayout from "../../layout/NavbarLayout";
+import { fetchFolderData } from "../../utils/apiUtils";
+import { supabase } from "../../config/supabaseClient";
+export default function Home(folderData) {
+  return (
+    <NavbarLayout>
+      <HomePage folderData={folderData} />
+    </NavbarLayout>
+  );
+}
 
-export default function Home() {
-  return <h1>Hello</h1>;
+export async function getServerSideProps(context) {
+  const supabase = createPagesServerClient(context);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return {
+      props: {},
+      redirect: {
+        permanent: false,
+        destination: "/guest",
+      },
+    };
+  }
+
+  const { data: folderData, error: folderDataError } = await supabase
+    .from("folder")
+    .select()
+    .eq("user_id", user.id);
+
+  return {
+    props: {
+      folderData,
+    },
+  };
 }
