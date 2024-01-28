762d7764c3f29d48eaf0a7327a22d9061d5d77fc
Nabeel Boda <needtoexplore01@gmail.com>
2024-01-28 14:52:13 +0530

    Feat: Redirect from '/' route

--- pages/guest.js
+++
@@ -1,6 +0,0 @@
-import { useEffect, useState } from "react";
-import supabase from "/config/supabaseClient";
-
-export default function Home() {
-  return <h1>Hello</h1>;
-}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
-export default function Home(folderData) {
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+
+export default function Home() {
   return <h1>Hello</h1>;
+}
+
+export async function getServerSideProps(ctx) {
+  const supabase = createPagesServerClient(ctx);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    return {
+      props: {},
+      redirect: {
+        permanent: false,
+        destination: "/home",
+      },
+    };
+  } else {
+    return {
+      props: {},
+      redirect: {
+        permanent: false,
+        destination: "/auth",
+      },
+    };
+  }
 }
