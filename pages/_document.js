06cf3837e25f5b710bbd050749c1427d5b55f24a
Nabeel Boda <needtoexplore01@gmail.com>
2024-01-28 17:03:26 +0530

    -

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Kzor PDF - New way of reading PDF with AI</title>
       <Head />
       <body>
         <Main />
